f611adb3fed3aba377663c19f108525aceb0d961
evan-martin <evanm689@gmail.com>
2022-07-22 12:09:35 -0700

    add default image for recipes with no image

--- src/components/recipe-card.jsx
+++ src/components/recipe-card.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
+import defaultRecipeImage from '../assets/default.jpg'
 import './component-styles/recipe-card.scss'
 
 function RecipeCard({ recipe, setRecipe }) {
 
+    const validateImageURL = (imageURL) => {
+        if (!imageURL) {
+          return defaultRecipeImage
+        } else return imageURL
+      };
+
     return (
         <Link to={recipe._id} onClick={() => { setRecipe(recipe) }} style={{ textDecoration: 'none' }} >
-            <img className='card-image' src={recipe.imageURL} />
+            <img className='card-image' src={validateImageURL(recipe.imageURL)} />
             <p className='card-title'>{recipe.name}</p>
         </Link>
     )
