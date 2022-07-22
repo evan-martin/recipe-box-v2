29e2ccf4ad04e666a1011071eb079724a04a3062
evan-martin <evanm689@gmail.com>
2022-07-22 11:57:54 -0700

    add card grid to homepage

--- src/components/recipe-card.jsx
+++ src/components/recipe-card.jsx
@@ -1,15 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import './component-styles/recipe-card.scss'
 
 function RecipeCard({ recipe, setRecipe }) {
 
     return (
-        <>
-            <Link to={recipe._id} onClick={() => { setRecipe(recipe) }} >
-                <div>
-                    {recipe.name}
-                </div>
-            </Link>
-        </>
+        <Link to={recipe._id} onClick={() => { setRecipe(recipe) }} style={{ textDecoration: 'none' }} >
+            <img className='card-image' src={recipe.imageURL} />
+            <p className='card-title'>{recipe.name}</p>
+        </Link>
     )
 }
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,19 @@
-import { Link } from "react-router-dom"
-import RecipeCard from "../components/recipe-card";
+import { Link } from 'react-router-dom'
+import RecipeCard from '../components/recipe-card';
+import './page-styles/home.scss'
 
 function Home({ data, setRecipe }) {
 
     return (
         <>
-            {data.map((recipe) => (
-                <div key={recipe._id}>
-                    <RecipeCard recipe={recipe} setRecipe={setRecipe} />
-                </div>
-            ))}
-            <Link to="/new-recipe">
+            <div className='grid'>
+                {data.map((recipe) => (
+                    <div className='grid-item' key={recipe._id}>
+                        <RecipeCard recipe={recipe} setRecipe={setRecipe} />
+                    </div>
+                ))}
+            </div>
+            <Link to='/new-recipe'>
                 <button>New Recipe</button>
             </Link>
         </>
