aac1ab8eaec73d33f127e29dfa295fafb6906e3c
evan-martin <evanm689@gmail.com>
2022-07-22 15:20:13 -0700

    add search bar and functionality

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,16 @@
+import useLocalStorage from '../hooks/useLocalStorage';
 import RecipeCard from '../components/recipe-card';
 import Fab from '@mui/material/Fab';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
 import './page-styles/home.scss'
 
+
 function Home({ data, setRecipe }) {
+
+    const [searchTerm, setSearchTerm] = useLocalStorage('searchTerm', '')
 
     const scrollToTop = () => {
         window.scrollTo({
@@ -12,15 +19,43 @@ function Home({ data, setRecipe }) {
         });
     };
 
+    const filteredRecipes = data.filter(recipe =>
+        recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+        || recipe.ingredients.toLowerCase().includes(searchTerm.toLowerCase())
+        || recipe.method.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    filteredRecipes.reverse()
+
     return (
+
         <>
+            <div className='search-bar'>
+                <TextField
+                    placeholder='Search'
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position='start'>
+                                <SearchIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                    variant='outlined'
+                    size='small'
+                    type='search'
+                    defaultValue={searchTerm.trim()}
+                    onChange={event => setSearchTerm(' ' + event.target.value)}
+                />
+            </div>
+
             <div className='grid'>
-                {data.map((recipe) => (
+                {filteredRecipes.map((recipe) => (
                     <div className='grid-item' key={recipe._id}>
                         <RecipeCard recipe={recipe} setRecipe={setRecipe} />
                     </div>
                 ))}
             </div>
+
             <div className='fab'>
                 <Fab color='secondary' onClick={scrollToTop}>
                     <ArrowUpwardIcon />
